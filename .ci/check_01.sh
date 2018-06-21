349f1e1bc2e19e774082787573f6d76450a2110c
Al Niessner <Al.Niessner@jpl.nasa.gov>
2018-06-21 10:32:36 -0700

    utility functions and PEP-8
    added .ci/util.sh as this is the basis for the rest. It has all of the helper functions in it need to make check_*.sh and build_*.sh simpler to read.
    
    add .ci/check_01.sh, which is that code meets PEP-8. Wronte some dummy stuff not in the repo to show that it works.

---
+++ .ci/check_01.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+
+. .ci/util.sh
+
+state="pending" # "success" "pending" "failure" "error"
+description="Coding standard checks of the excalibur software"
+context="continuous-integration/01/esp-PEP8"
+
+post_state "$context" "$description" "$state"
+
+if current_state
+then
+    pep8 --ignore=E24,E121,E123,E124,E126,E127,E211,E225,E226,E231,E301,E302,E402,E501,E701,E702,E704 \
+         --statistics ${PWD} | tee ${PWD}/pep8.rpt.txt
+    errs=`wc -l pep8.rpt.txt | awk '{print $1}'`
+    [ $errs -ne 0 ] && echo -n "failure" > .ci/status.txt
+    state=`get_state`
+fi
+
+post_state "$context" "$description" "$state"
---
+++ .ci/util.sh
@@ -0,0 +1,76 @@
+
+basedir=$(cd "$(dirname "$0")"/..; pwd)
+GHE4JPL_API_URL=https://github-fn.jpl.nasa.gov/api/v3
+GHE4JPL_TOKEN=fe03c6cd731021805fc89fde8fb1be6b3432ae51
+ghrVersion=${ghrVersion:-"`git describe --tags`"}
+PATH=/usr/local/python3/bin:${PATH}
+REPO=EXCALIBUR/esp
+
+export GHE4JPL_API_URL PATH 
+
+current_state ()
+{
+    #test `cat ${basedir}/.ci/status.txt` == "success"
+    test 'a' == 'a'
+}
+
+destinations ()
+{
+    grep -v '#' ${basedir}/app.dependency.manifest | awk -e '{print $1}'
+}
+
+download ()
+{
+    curl -L \
+         -H "Authorization: token ${GHE4JPL_TOKEN}" \
+         ${ghrReleaseTarball} > $1
+}
+
+get_state ()
+{
+    cat ${basedir}/.ci/status.txt
+}
+
+lookup ()
+{
+    grep -v '#' ${basedir}/app.dependency.manifest | grep ${1}
+}
+
+lookup_source ()
+{
+    lookup $1 | awk -e '{print $2}'
+}
+
+lookup_version ()
+{
+    lookup $1 | awk -e '{print $3}'
+}
+
+post_state ()
+{
+    if current_state && [ "$3" == "pending" ]
+    then
+        echo "$1 -- $2"
+    else
+        echo "$1 -- completion state: $3"
+    fi
+
+    curl -XPOST \
+         -H "Authorization: token ${GHE4JPL_TOKEN}" \
+         ${GHE4JPL_API_URL}/repos/${REPO}/statuses/${ghprbActualCommit} \
+         -d "{\"state\": \"${3}\", \"target_url\": \"${BUILD_URL}/console\", \"description\": \"${2}\", \"context\": \"${1}\"}" > /dev/null 2>&1
+}
+
+which_port ()
+{
+    python3 <<EOF
+v = "${1:-${ghrVersion}}".split ('.')
+if len (v) == 3:
+    if v[0].isdigit() and v[1].isdigit() and v[2].isdigit(): port = 16003
+    elif v[0].isdigit() and v[1].isdigit() and 0 < v[2].find ('-rc') and v[2].split('-')[0].isdigit(): port = 16002
+    else: port = 16001
+else: port = 16001
+
+print (port)
+EOF
+}
