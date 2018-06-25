787cef382427d7f3064d04156d8d978eb64288a9
Gael M Roudier <Gael.M.Roudier@jpl.nasa.gov>
2018-06-24 19:48:36 -0700

    Added transit.normalization (#11)

--- excalibur/transit/core.py
+++ excalibur/transit/core.py
@@ -4,6 +4,7 @@ import pdb
 import excalibur.system.core as syscore
 
 import numpy as np
+import lmfit as lm
 import matplotlib.pyplot as plt
 # ------------- ------------------------------------------------------
 # -- SV VALIDITY -- --------------------------------------------------
@@ -19,26 +20,58 @@ def norm(cal, tme, fin, out, selftype, verbose=False, debug=False):
     normed = False
     priors = fin['priors'].copy()
     ssc = syscore.ssconstants()
-    spectra = cal['data']['SPECTRUM']
-    wave = cal['data']['WAVE']
+    spectra = np.array(cal['data']['SPECTRUM'])
+    wave = np.array(cal['data']['WAVE'])
+    time = np.array(cal['data']['TIME'])
     vrange = cal['data']['VRANGE']
     if len(tme[selftype]) > 0:
         for p in tme['data'].keys():
+            out['data'][p] = {}
             rpors = priors[p]['rp']/priors['R*']*ssc['Rjup/Rsun']
             ignore = tme['data'][p]['ignore']
             orbits = tme['data'][p]['orbits']
             dvisits = tme['data'][p]['dvisits']
             visits = tme['data'][p]['visits']
             z = tme['data'][p]['z']
+            out['data'][p]['vrange'] = vrange
+            out['data'][p]['visits'] = []
+            out['data'][p]['nspec'] = []
+            out['data'][p]['wave'] = []
+            out['data'][p]['time'] = []
             for v in tme[selftype]:
                 selv = (dvisits == v) & (~ignore)
-                selvoot = selv & (abs(z) > 1e0 + rpors)
-                vspec = [s for s,sv in zip(spectra, selv)
-                         if sv == True]
-                vwave = [w for w,sv in zip(wave, selv)
-                         if sv == True]
-                if len(vwave) > 0:                    
+                selvoot = selv & (abs(z) > (1e0 + rpors))
+                if np.sum(selv) > 0:
+                    out['data'][p]['visits'].append(v)
+                    scores = [np.nansum(s)
+                              for s,sv in zip(spectra, selvoot)
+                              if sv == True]
+                    thrit = np.nanpercentile(scores, 75,
+                                             interpolation='higher')
+                    it = scores.index(thrit)
+                    template = spectra[selvoot][it]
+                    nspectra = [s/template for s in spectra[selv]]
+                    nspectra = np.array(nspectra)
+                    out['data'][p]['nspec'].append(nspectra)
+                    out['data'][p]['wave'].append(wave[selv])
+                    out['data'][p]['time'].append(time[selv])
+                    if verbose:
+                        plt.figure()
+                        for w,s in zip(wave[selv], nspectra):
+                            select = ((w > np.min(vrange)) &
+                                      (w < np.max(vrange)))
+                            plt.plot(w[select], s[select], 'o')
+                            pass
+                        plt.ylabel('Normalized Spectra')
+                        plt.xlabel('Wavelength [$\mu$m]')
+                        plt.xlim(np.min(vrange), np.max(vrange))
+                        plt.show()
+                        pass
                     pass
+                pass
+            if len(out['data'][p]['visits']) > 0:
+                normed = True
+                out['STATUS'].append(True)
                 pass
             pass
         pass
--- excalibur/transit/states.py
+++ excalibur/transit/states.py
@@ -1,7 +1,12 @@
 # -- IMPORTS -- ------------------------------------------------------
+import io
+
 import dawgie
 
 import excalibur
+
+import numpy as np
+import matplotlib.pyplot as plt
 # ------------- ------------------------------------------------------
 # -- SV -- -----------------------------------------------------------
 class NormSV(dawgie.StateVector):
@@ -18,6 +23,27 @@ class NormSV(dawgie.StateVector):
 
     def view(self, visitor:dawgie.Visitor)->None:
         if self['STATUS'][-1]:
+            for p in self['data'].keys():
+                vrange = self['data'][p]['vrange']
+                for v in self['data'][p]['visits']:
+                    index = self['data'][p]['visits'].index(v)
+                    wave = self['data'][p]['wave'][index]
+                    nspec = self['data'][p]['nspec'][index]
+                    myfig = plt.figure()
+                    for w,s in zip(wave, nspec):
+                        select = ((w > np.min(vrange)) &
+                                  (w < np.max(vrange)))
+                        plt.plot(w[select], s[select], 'o')
+                        pass
+                    plt.ylabel('Normalized Spectra')
+                    plt.xlabel('Wavelength [$\mu$m]')
+                    plt.xlim(np.min(vrange), np.max(vrange))
+                    buf = io.BytesIO()
+                    myfig.savefig(buf, format='png')
+                    visitor.add_image('...', ' ', buf.getvalue())
+                    plt.close(myfig)
+                    pass
+                pass
             pass
         pass
 # -------- -----------------------------------------------------------
