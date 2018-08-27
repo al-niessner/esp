46bda5a5624f422de0cb846ecd837879893c111f
Al Niessner <Al.Niessner@jpl.nasa.gov>
2018-08-27 15:33:34 -0700

    add some meat to the dockerfiles

--- .ci/Dockerfile.py
+++ .ci/Dockerfile.py
@@ -1,1 +1,7 @@
 FROM dawgie:ghrVersion
+RUN set -ex && \
+    apt-get update && \
+    apt-get install -y libblas3 libblas-dev python-liblas && \
+    apt-get clean && apt-get autoremove && \
+    /usr/bin/pip3 install astropy ldtk lmfit matplotlib scipy pymc && \
+   rm -rf ${HOME}/.cache
