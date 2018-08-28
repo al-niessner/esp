23a530ac1d80670a0fe06d8287aaa95e8f6406e4
Al Niessner <Al.Niessner@jpl.nasa.gov>
2018-08-28 10:33:39 -0700

    dockerized the CI checks

--- .ci/check_01.sh
+++ .ci/check_01.sh
@@ -10,7 +10,8 @@ post_state "$context" "$description" "$state"
 
 if current_state
 then
-    pep8 --ignore=E24,E121,E123,E124,E126,E127,E211,E225,E226,E231,E301,E302,E402,E501,E701,E702,E704 \
+    docker run --rm -v $PWD:$PWD -u $UID -w $PWD esp_cit:$(cit_version) pycodestyle \
+           --ignore=E24,E121,E123,E124,E126,E127,E211,E225,E226,E231,E252,E301,E302,E305,E402,E501,W504,E701,E702,E704,E722,E741 \
          --statistics ${PWD} | tee ${PWD}/pep8.rpt.txt
     errs=`wc -l pep8.rpt.txt | awk '{print $1}'`
     [ $errs -ne 0 ] && echo -n "failure" > .ci/status.txt
