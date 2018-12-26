d90c9ba496eeb368683ac5f89031a27f92e8cb9d
Al Niessner <Al.Niessner@jpl.nasa.gov>
2018-12-26 15:32:21 -0800

    have to move the deploys up one to squeeze in a standard python base

--- .ci/deploy_02.sh
+++ .ci/deploy_02.sh
@@ -3,36 +3,31 @@
 . .ci/util.sh
 
 state="pending" # "success" "pending" "failure" "error"
-description="Build a worker to execute the AE"
-context="continuous-integration/01/esp-build-worker"
+description="Build a server to manage the AE"
+context="continuous-integration/01/esp-deploy-server"
 
 post_state "$context" "$description" "$state"
 
 if current_state
 then
-    if [ -n "$(docker images | grep esp_worker | grep latest)" ]
-    then
-        docker rmi esp_worker:latest
-    fi
-
-    lv=$(layer_versions)
-    version="${lv:0:16}"
-    esp_git_rev="$(git rev-parse HEAD)"
-    rm .ci/Dockerfile.1 .ci/Dockerfile.2
-    python3 <<EOF
-with open ('.ci/Dockerfile.worker', 'rt') as f: text = f.read()
-with open ('.ci/Dockerfile.1', 'tw') as f: f.write (text.replace ("ghrVersion", "${version}").replace ('esp-git-rev', "${esp_git_rev}"))
-with open ('setup.py', 'rt') as f: text = f.read()
-with open ('setup.py', 'tw') as f: f.write (text.replace ('esp-git-rev', "${esp_git_rev}"))
-EOF
-    .ci/dcp.py --server .ci/Dockerfile.1 &
-    while [ ! -f .ci/Dockerfile.1.dcp ]
+    for destination in `destinations`
     do
-        sleep 3
+        if [ "$destination" == "dawgie" ]
+        then
+            source="`lookup_source $destination`"
+            version="`lookup_version $destination`"
+            
+            if [ -z "$(docker images | grep esp_server:${version})" ]
+            then
+               python3 <<EOF
+with open ('.ci/Dockerfile.server', 'rt') as f: text = f.read()
+with open ('.ci/Dockerfile.1', 'tw') as f: f.write (text.replace ("ghrVersion", "${version}"))
+EOF
+               docker build --network=host -t esp_server:${version} - < .ci/Dockerfile.1
+               rm .ci/Dockerfile.1 
+            fi
+        fi
     done
-    docker build --network=host -t esp_worker:latest - < .ci/Dockerfile.1.dcp
-    git checkout setup.py
-    rm .ci/Dockerfile.1 .ci/Dockerfile.1.dcp
     state=`get_state`
 fi
 
