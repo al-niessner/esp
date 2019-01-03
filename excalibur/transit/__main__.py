a5eb7e5e3f4444c436c0a82ac5ba2aa3a53b5faf
groudier <gael.m.roudier@jpl.nasa.gov>
2019-01-03 15:03:13 -0800

    before pull

--- excalibur/transit/__main__.py
+++ excalibur/transit/__main__.py
@@ -9,7 +9,11 @@ import excalibur.transit.bot
 # ------------- ------------------------------------------------------
 rid = int(os.environ.get('RUNID', None))
 tn = os.environ.get('TARGET_NAME', None)
-dawgie.context._ports(int(os.environ.get('FE_PORT', None)))
+#dawgie.context._ports(int(os.environ.get('FE_PORT', None)))
+print(dawgie.context.db_impl)
+print(dawgie.context.gpg_home)
+#dawgie.context.fe_port = int(19990)
+dawgie.context.db_port = int(8083)
 dawgie.security.initialize(os.path.expandvars(os.path.expanduser
                                               (dawgie.context.gpg_home)))
 dawgie.db.reopen()
