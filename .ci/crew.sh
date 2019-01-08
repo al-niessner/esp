32b6636c3775e3e74a61b8730067c3550370fe2c
Al Niessner <Al.Niessner@jpl.nasa.gov>
2019-01-08 12:34:56 -0800

    changes to keep the crew running

--- .ci/crew.sh
+++ .ci/crew.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-while [ -f /tmp/crew.ops ]
-do
-    flock /tmp/crew.ops $(dirname $0)/spawn.sh
-done
+if [ -f /tmp/crew.ops ]
+then
+    flock --unlock /tmp/crew.ops $(dirname $0)/spawn.sh
+fi
--- .ci/spawn.sh
+++ .ci/spawn.sh
@@ -4,7 +4,7 @@ for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16
 do
     if [ -z "$(docker ps | grep ops_worker_$i)" ]
     then
-        $(dirname $0)/worker.sh $i
+        $(dirname $0)/worker.sh $i &
     fi
 done
 
