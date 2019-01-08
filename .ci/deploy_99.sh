91ec36c79524f49265d2e8c0fc88e74acb65b0ad
Al Niessner <Al.Niessner@jpl.nasa.gov>
2019-01-08 15:19:05 -0800

    might as well move the server about too

--- .ci/deploy_99.sh
+++ .ci/deploy_99.sh
@@ -3,7 +3,7 @@
 . .ci/util.sh
 
 state="pending" # "success" "pending" "failure" "error"
-description="Deploy the new AE (base, cit, devel, tools, worker)"
+description="Deploy the new AE (base, cit, devel, server, tools, worker)"
 context="continuous-integration/99/esp-deploy"
 
 post_state "$context" "$description" "$state"
@@ -15,13 +15,14 @@ version="$(lookup_version dawgie)"
 [ -z "$(docker images | grep "esp_cit *${cit_version}")" ] && ( echo -n 'failure' > .ci/status.txt ; msg='CIT not found' ) || ( msg='CIT found' )
 [ -z "$(docker images | grep "esp_devel *${version}")" ] && ( echo -n 'failure' > .ci/status.txt  ; msg='${msg}\nDEV not found' ) || ( msg='${msg}\nDEV found')
 [ -z "$(docker images | grep "esp_py *${py_version}")" ] && ( echo -n 'failure' > .ci/status.txt  ; msg='${msg}\nPY not found' ) || ( msg='${msg}\nPY found')
+[ -z "$(docker images | grep "esp_server *${version}")" ] && ( echo -n 'failure' > .ci/status.txt  ; msg='${msg}\nSERVER not found' ) || ( msg='${msg}\nSERVER found')
 [ -z "$(docker images | grep "esp_tools *${version}")" ] && ( echo -n 'failure' > .ci/status.txt  ; msg='${msg}\nTOOLS not found' ) || ( msg='${msg}\nTOOLS found')
 [ -z "$(docker images | grep "esp_worker *latest")" ] && ( echo -n 'failure' > .ci/status.txt  ; msg='${msg}\nWORKER not found' ) || ( msg='${msg}\nWORKER found')
 
 if current_state
 then
     tempfn=$(mktemp -p /proj/sdp/data/stg)
-    docker save -o ${tempfn} esp_cit:${cit_version} esp_devel:${version} esp_py:${py_version} esp_tools:${version} esp_worker:latest
+    docker save -o ${tempfn} esp_cit:${cit_version} esp_devel:${version} esp_py:${py_version} esp_server:${version} esp_tools:${version} esp_worker:latest
     ssh mentor0 docker rmi esp_cit:${cit_version} esp_devel:${version} esp_py:${py_version} esp_tools:${version} esp_worker:latest
     ssh mentor1 docker rmi esp_cit:${cit_version} esp_devel:${version} esp_py:${py_version} esp_tools:${version} esp_worker:latest
     ssh mentor2 docker rmi esp_cit:${cit_version} esp_devel:${version} esp_py:${py_version} esp_tools:${version} esp_worker:latest
