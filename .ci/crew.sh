0ca75a201c3b4b6d69313ee55dc366c63cba18aa
Al Niessner <Al.Niessner@jpl.nasa.gov>
2019-01-09 10:20:57 -0800

    more cleanup of crew

--- .ci/crew.sh
+++ .ci/crew.sh
@@ -4,15 +4,13 @@ while [ -f /tmp/crew.ops ]
 do
     for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16
     do
-        if [ -z "$(docker ps | grep ops_worker_$i)" ]
+        existance="$(docker ps -a | grep ops_worker_$i)"
+        echo "existance of ops_worker_${i}: ${existance}"
+
+        if [ -z "${existance}" ]
         then
             $(dirname $0)/worker.sh $i
             docker ps | grep ops_worker_$i
-            while [ -z "$([ -f /tmp/crew.ops ]  && ( docker ps | grep ops_worker_$i ) || echo abort)" ]
-            do
-                echo "waiting for ops_worker_$i to dawn"
-                sleep 2
-            done
         fi
     done
     echo 'sleeping until next check'
