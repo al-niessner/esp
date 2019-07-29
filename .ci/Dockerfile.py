8730f9bd6ee6c863de07d9ac9717b6aef1f8b78e
Al Niessner <Al.Niessner@jpl.nasa.gov>
2019-07-29 12:07:02 -0700

    change dependency to pip instead of docker

--- .ci/Dockerfile.py
+++ .ci/Dockerfile.py
@@ -1,6 +1,24 @@
-FROM dawgie:ghrVersion
+FROM ubuntu:18.04
 RUN set -ex && \
+    chmod 755 /bin/dcp.py && \
+    export DEBIAN_FRONTEND=noninteractive && \
+    apt-get update && \
+    apt-get install -y \
+            git \
+            graphviz \
+            haveged \
+            python3 \
+            python3-dev \
+            python3-gdbm \
+            python3-numpy \
+            python3-pip \
+            python3-psycopg2 \
+            python3-setuptools && \
+    apt-get clean && apt-get autoremove && \
+    ln -fs /usr/share/zoneinfo/America/Los_Angeles /etc/localtime && \
+    dpkg-reconfigure --frontend noninteractive tzdata && \
     /usr/bin/pip3 install astropy==3.0.4 \
+                          dawgie==1,2.3 \
                           ldtk==1.0 \
                           lmfit==0.9.11 \
                           matplotlib==2.2.3 \
