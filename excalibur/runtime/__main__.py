6ebbb182ac91e8601d931d73a3a2ab93fbf3a548
Al Niessner <Al.Niessner@xxx.xxx>
2024-03-22 09:01:12 -0700

    pep8 and pylint compliant

--- excalibur/runtime/__main__.py
+++ excalibur/runtime/__main__.py
@@ -3,8 +3,11 @@
 import dawgie.db
 import excalibur.runtime.algorithms
 
+# need to fake some dawgie stuff so that can run the unit independent of any
+# pipeline since it is the data in the configuration file that matters not any
+# data or state of the pipeline itself. There going to
+# pylint: disable=missing-class-docstring,missing-function-docstring
 class FakeDawgie:
-    '''keep the test runnable without needing a pipeline'''
     def __init__(self):
         self.__name = 'runtime'
     def _bot(self): return self
@@ -17,8 +20,8 @@ def connect(_alg, _bit, _tn):
     return FakeDawgie()
 
 def targets():
-    '''as not require a real DB running'''
     return ['a','b','c', 'GJ 3193', 'GJ 3193 (taurex sim @TS)']
+# pylint: enable=missing-class-docstring,missing-function-docstring
 
 setattr (dawgie.db, 'connect', connect)
 setattr (dawgie.db, 'targets', targets)
