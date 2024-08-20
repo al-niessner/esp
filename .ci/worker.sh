b77be0abe95dae16f96a1909e751c482fc14b92b
Albert F Niessner <Albert.F.Niessner@jpl.nasa.gov>
2024-08-20 08:48:50 -0700

    Update worker.sh (#678)
    need to reference host in cert

--- .ci/worker.sh
+++ .ci/worker.sh
@@ -9,4 +9,4 @@ docker run \
        --network host \
        -u $UID:$GROUPS \
        -v /proj/sdp/data:/proj/data -v ${HOME}/.gnupg:/proj/gnupg \
-       esp_worker:latest -i $1 -n mentor0.jpl.nasa.gov -p 8081 > /dev/null
+       esp_worker:latest -i $1 -n excalibur.jpl.nasa.gov -p 8081 > /dev/null
