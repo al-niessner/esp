0be01680cb2b5ec3b66a1bdac97a97d90fd4c3c4
Gael M Roudier <Gael.M.Roudier@jpl.nasa.gov>
2024-12-10 14:34:09 -0800

    696 follow up on jwst after 2024 internships (#712)
    * oops

--- excalibur/target/core.py
+++ excalibur/target/core.py
@@ -798,13 +798,13 @@ def mastapi(tfl, out, dbs, download_url=None, hst_url=None, verbose=False):
                 allraw.extend(rawdata)
                 # --<
                 # !!! TEMPORARY !!!
-                allsci.extend(scidata)
-                allmiss.extend([obscol]*len(scidata))
-                allurl.extend([thisurl]*len(scidata))
+                # allsci.extend(scidata)
+                # allmiss.extend([obscol]*len(scidata))
+                # allurl.extend([thisurl]*len(scidata))
                 pass
-            # allsci.extend(scidata)
-            # allmiss.extend([obscol]*len(scidata))
-            # allurl.extend([thisurl]*len(scidata))
+            allsci.extend(scidata)
+            allmiss.extend([obscol]*len(scidata))
+            allurl.extend([thisurl]*len(scidata))
             # >--
             if verbose: log.warning('%s: %s: %s', obscol, o, len(scidata))
             pass
