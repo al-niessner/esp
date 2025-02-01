64e641f220433360e9be73e40dbccb680e550d8d
Albert F Niessner <Albert.F.Niessner@jpl.nasa.gov>
2025-01-31 16:18:47 -0800

    upgrade for TLS sockets (#724)

--- .ci/worker.sh
+++ .ci/worker.sh
@@ -10,5 +10,9 @@ docker run \
        --name ops_worker_$1 \
        --network host \
        -u $UID:$GROUPS \
-       -v /proj/sdp/data:/proj/data -v ${HOME}/.ssh:/proj/mycerts:ro \
+       -v /proj/sdp/data:/proj/data \
+       -v ${HOME}/.ssh:/proj/mycerts:ro \
+       -v /etc/ssl:/etc/ssl:ro \
+       -v /usr/local/share/ca-certificates:/usr/local/share/ca-certificates:ro \
+       -v /usr/share/ca-certificates:/usr/share/ca-certificates:ro 
        esp_worker:latest -i $1 -n excalibur.jpl.nasa.gov -p 8081 > /dev/null
--- bash/pp_reset.sh
+++ bash/pp_reset.sh
@@ -4,4 +4,4 @@ myname="${DAWGIE_SSL_PEM_MYNAME:-excalibur.jpl.nasa.gov}"
 myself="${DAWGIE_SSL_PEM_MYSELF:-${HOME}/.ssh/excalibur_identity.pem}"
 let priv_port=${1:-${DAWGIE_FE_PORT:-8080}}+5
 
-curl -XPOST --cert ${myself} "https://${myname}:${priv_port}/app/reset"  # &archive=${DAWGIE_ARCHIVE:-true}
+curl -XPOST --cert ${myself} "https://${myname}:${priv_port}/app/reset&archive=${DAWGIE_ARCHIVE:-true}"
