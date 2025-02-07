f5f89875a558962f5683c13ed6652add50665b4b
Albert F Niessner <Albert.F.Niessner@jpl.nasa.gov>
2025-02-07 09:32:42 -0800

    Update setup.py (#734)
    have to leave this for backward compat right now

--- setup.py
+++ setup.py
@@ -3,7 +3,13 @@
 import os
 import setuptools
 
-excalibur = os.path.join ('excalibur', '__init__.py')
+deps = ['astropy==3.0.4',
+        'ldtk==1.0',
+        'lmfit==0.9.11',
+        'matplotlib==2.2.3',
+        'pymc3==3.5',
+        'scipy==1.1.0',
+        ]excalibur = os.path.join ('excalibur', '__init__.py')
 version = 'esp-git-rev'
 with open (os.path.join (os.path.dirname (__file__), excalibur)) as f: t = f.read()
 t = t.replace ('${UNDEFINED}', version)
